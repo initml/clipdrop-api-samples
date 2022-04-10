6dc617e1a5466a161e98de5ef253937a915309ee
Cyril Diagne <cyril@initml.co>
2022-04-10 18:39:07 +0200

    change api key field type

--- remove-background-bulk/src/APIKeyInput.tsx
+++ remove-background-bulk/src/APIKeyInput.tsx
@@ -10,6 +10,7 @@ export default function APIKeyInput(props: any) {
         id="apikey"
         name="apikey"
         autoComplete="API Key"
+        type="password"
         required
         className="appearance-none relative block w-full my-4 px-3 py-3 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
         placeholder="Paste your ClipDrop API Key here to get started"
