60bbdb2157b17f65a609c964584b1363a05e3917
Igor ESCHALIER <igor@initml.co>
2022-06-06 14:55:01 +0200

    feat(cli): remove-background api call

--- CLI/clipdrop-cli/src/Constants.ts
+++ CLI/clipdrop-cli/src/Constants.ts
@@ -3,3 +3,5 @@ export const STORE_API_KEY = 'api-key'
 
 export const FUNCTION_SUPER_RESOLUTION =
   'https://apis.clipdrop.co/super-resolution/v1'
+export const FUNCTION_REMOVE_BACKGROUND =
+  'https://apis.clipdrop.co/remove-background/v1'
