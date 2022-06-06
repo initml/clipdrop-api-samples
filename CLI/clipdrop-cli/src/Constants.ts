2af48f5e445d01929832d1f73083e0cb4ccff34c
Igor ESCHALIER <igor@initml.co>
2022-06-06 15:20:02 +0200

    feat(cli): cleanup api call

--- CLI/clipdrop-cli/src/Constants.ts
+++ CLI/clipdrop-cli/src/Constants.ts
@@ -5,3 +5,4 @@ export const FUNCTION_SUPER_RESOLUTION =
   'https://apis.clipdrop.co/super-resolution/v1'
 export const FUNCTION_REMOVE_BACKGROUND =
   'https://apis.clipdrop.co/remove-background/v1'
+export const FUNCTION_CLEANUP = 'https://apis.clipdrop.co/cleanup/v1'
