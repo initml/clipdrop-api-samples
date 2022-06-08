63877710314352fe3205d1d4eaf78a82230a25da
Damien Henry <damienhenry@Damiens-MacBook-Pro.local>
2022-06-08 17:42:37 +0200

    initial version, only background removal tested for now

---
+++ python/clipdrop.py
@@ -0,0 +1,188 @@
+# Process files use ClipAPI
+
+import argparse
+import io
+import json
+import math
+import os
+import shutil
+import sys
+import time
+import zipfile
+from ast import Str
+from configparser import ExtendedInterpolation
+from os import listdir
+from os.path import isfile, join
+
+# import cv2
+import numpy as np
+import PIL
+import PIL.ImageOps
+import requests
+from PIL import Image
+
+
+parser = argparse.ArgumentParser(
+    description='This tool can process images using the ClipAPI.')
+parser.add_argument('--API_KEY', required=True, type=str,
+                    help='The API key to use for the API call.')
+parser.add_argument('--api', required=True, choices=('remove-background', 'image-upscaler'),
+                    help='The alogrythm to be applyed on all your images')
+parser.add_argument('-i', '--input', required=True,
+                    help='the folder containing the images to process')
+parser.add_argument('-o', '--output', required=True,
+                    help='the output folder')
+parser.add_argument('--upscale', type=int, default=2,
+                    help='the upscale ratio, can be 2 or 4')
+parser.add_argument('--output_format',
+                    choices=('png', 'jpeg', 'webp'),
+                    default='png',
+                    help='Select the new background')
+parser.add_argument('--background_color',
+                    type=str,
+                    default=None
+                    help='Select the new background color, can be any color, or transparent or chekcboard')
+parser.add_argument('--width', type=int, default=None,
+                    help='width of the output image')
+parser.add_argument('--height', type=int, default=None,
+                    help='height of the output image')
+parser.add_argument('--center', action='store_true', default=False,
+                    help='center the image by removing transparent pixels')
+parser.add_argument('--join', action='store_true', default=False,
+                    help='center the image')
+
+args = parser.parse_args()
+
+
+# API urls and Key
+SUPER_RESOLUTION = 'https://apis.clipdrop.co/super-resolution/v1'
+REMOVE_BACKGROUND = 'https://apis.clipdrop.co/remove-background/v1'
+API_KEY = args.API_KEY
+
+
+def remove_background(input_file, output_file):
+    output_format = os.path.splitext(output_file)[1].lower()
+    img_in = open(input_file, 'rb')
+    files = {'image_file': ('image.jpeg', img_in, 'image/jpeg')}
+    image_type = {'.png': 'image/png', '.jpeg': 'image/jpeg',
+                  '.jpg': 'image/jpeg', '.webp': 'image/webp'}[output_format.lower()]
+    headers = {'x-api-key': API_KEY,
+               'accept': image_type}
+    r = requests.post(REMOVE_BACKGROUND,
+                      stream=True,
+                      files=files,
+                      headers=headers)
+
+    if r.ok:
+        with open(output_file, 'wb') as f:
+            for chunk in r:
+                f.write(chunk)
+
+        # zip = zipfile.ZipFile(temp_file)
+        # os.makedirs(filename_out, exist_ok=True)
+        # zip.extractall(filename_out)
+        # os.remove(temp_file)
+    else:
+        r.raise_for_status()
+
+
+def checkerboard(h, w, channels=3, tiles=16, fg=.95, bg=.6):
+    """Create a shape (w,h,1) array tiled with a checkerboard pattern."""
+    # print('input',h,w)
+    square_size = [math.ceil(float(d / tiles) / 2) for d in [h, w]]
+    # print ('square size',square_size)
+    board = [[fg, bg] * tiles, [bg, fg] * tiles] * tiles
+    # print ('board',board)
+    scaled = np.kron(board, np.ones(square_size))
+    # print('scaled', scaled.shape)
+    scaled = scaled[:h, :w]
+    # print('scaled', scaled.shape)
+    return Image.fromarray(np.uint8(np.dstack([scaled]*channels)*255))
+
+
+def composite(image_in, mask_in, f_out, color='white'):
+    if color == 'transparent':
+        img = Image.open(f_in).convert('RGBA')
+        mask = Image.open(mask_in)
+        mask = PIL.ImageOps.invert(mask)
+        mask = mask.resize(img.size, Image.BICUBIC)
+        transparent = Image.new('RGBA', img.size, color=(0, 0, 0, 0))
+        # print(f_in, mask_in, f_out, img.size, mask.size)
+        im = Image.composite(transparent, img, mask).convert('RGBA')
+        im.save(f_out, "PNG")
+    elif color == 'checkerboard':
+        img = Image.open(image_in)
+        mask = Image.open(mask_in)
+        mask = PIL.ImageOps.invert(mask)
+        checker = checkerboard(h=img.size[1], w=img.size[0])
+        im = Image.composite(checker, img, mask)
+        im.save(f_out)
+    else:
+        img = Image.open(image_in)
+        mask = Image.open(mask_in)
+        mask = PIL.ImageOps.invert(mask)
+        background = Image.new('RGB', img.size, color=color)
+        im = Image.composite(background, img, mask)
+        im.save(f_out)
+
+
+def resize(f_in, f_out, new_size):
+    img = Image.open(f_in)
+    img.thumbnail(new_size, Image.ANTIALIAS)
+    img = img.convert("RGB")
+    img.save(f_out)
+
+
+def join_imgs(imgs, filename_out):
+    images = [Image.open(x) for x in imgs]
+    widths, heights = zip(*(i.size for i in images))
+
+    total_width = sum(widths)
+    max_height = max(heights)
+
+    new_im = Image.new('RGB', (total_width, max_height))
+
+    x_offset = 0
+    for im in images:
+        new_im.paste(im, (x_offset, 0))
+        x_offset += im.size[0]
+    new_im.save(filename_out)
+
+
+# Define all needed folders
+file_system = {
+    'in': args.input,
+    'out': args.output,
+}
+
+# Generate folders if needed
+for key, folder in file_system.items():
+    if not os.path.exists(folder):
+        print('Creating ', folder)
+        os.makedirs(folder)
+
+################################ Process files ###########################################
+files = [f for f in listdir(file_system['in'])
+         if isfile(join(file_system['in'], f))]
+for file in files:
+    try:
+        print(f'Start processing {file}')
+        start = time.time()
+        file_in = join(file_system['in'], file)
+        name = os.path.splitext(file)[0]
+        ext = args.output_format
+        file_out = join(file_system['out'], f'{name}.{ext}')
+        if args.api == 'remove-background':
+            remove_background(file_in, file_out)
+
+        end = time.time()
+        print(f'{file} processed in {end - start} seconds')
+
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
+    except BaseException as error:
+        print('Error with {}, {}'.format(file_system['in'], error))
