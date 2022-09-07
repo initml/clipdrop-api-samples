60c1ce4f4e77632df5e90191853c43aa47ce9599
Jonathan Blanchet <john@initml.co>
2022-09-07 16:54:38 +0200

    Update code for Images from CDN

--- web/profile-pictures/components/Galery.tsx
+++ web/profile-pictures/components/Galery.tsx
@@ -1,48 +1,48 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
+import Image from './Image'
 
 type GalleryProps = {
-  choices: string[];
-  choiceSelection: Function;
-};
+  choices: string[]
+  choiceSelection: Function
+}
 
 const Gallery: React.FC<GalleryProps> = ({
   choices,
   choiceSelection,
 }: GalleryProps): JSX.Element => {
-  const [selection, setSelection] = useState<string | undefined>(undefined);
+  const [selection, setSelection] = useState<string | undefined>(undefined)
 
   const handleClick = (choice: string): void => {
-    choiceSelection(choice);
-    setSelection(choice);
-  };
+    choiceSelection(choice)
+    setSelection(choice)
+  }
 
   return (
     <>
-      <div className='gallery'>
-        {choices.map(
-          (choice: string, index: number): JSX.Element => {
-            return (
-              <p
-                key={index}
-                style={{
-                  backgroundColor: choice == selection ? '#0062cc' : 'white',
-                }}
-                onClick={(): void => {
-                  handleClick(choice);
-                }}
-              >
-                <img
-                  src={"/thumbnails/" +  choice + ".png"}
-                  alt={choice}
-                >
-                </img>
-              </p>
-            );
-          }
-        )}
+      <div className="gallery">
+        {choices.map((choice: string, index: number): JSX.Element => {
+          return (
+            <p
+              key={index}
+              style={{
+                backgroundColor: choice == selection ? '#0062cc' : 'white',
+              }}
+              onClick={(): void => {
+                handleClick(choice)
+              }}
+            >
+              <Image
+                width={100}
+                height={100}
+                src={'profile-picture/templates/' + choice + '.png'}
+                alt={`Preview of template ${choice}`}
+              ></Image>
+            </p>
+          )
+        })}
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Gallery;
+export default Gallery
